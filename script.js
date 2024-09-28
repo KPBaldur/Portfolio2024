6bd7a5299dc0fc5a20d61cc16a55cbcac146ce93
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-09-28 14:51:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ slider.addEventListener('mouseover', () => {
 slider.addEventListener('mouseout', () => {
     slider.style.animationPlayState = 'running';
 });
+
+document.querySelector('.read-more-btn').addEventListener('click', function() {
+    const text = document.querySelector('.about-me-text');
+    text.classList.toggle('expanded');
+    this.textContent = text.classList.contains('expanded') ? 'Mostrar Menos' : 'Saber Mas';
+  });
+  
+  document.querySelector('.show-pdf-btn').addEventListener('click', function() {
+    window.open('/path/to/your/pdf.pdf', '_blank');
+  });
+  
+  document.querySelector('.show-experience-btn').addEventListener('click', function() {
+    window.open('/path/to/experience/page', '_blank');
+  });
