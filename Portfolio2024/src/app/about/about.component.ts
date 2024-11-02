432d86b4c2946f1179712326dac2404a7bf9d03b
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-11-02 20:17:37 -0300

    Update about.component.ts

--- Portfolio2024/src/app/about/about.component.ts
+++ Portfolio2024/src/app/about/about.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  ngOnInit(): void {
+    document.
+  }
 }
