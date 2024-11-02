65fb273b448c46cfd47bbf7a5828a37a98f5c0f9
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-11-02 20:17:42 -0300

    Update home.component.ts

--- Portfolio2024/src/app/home/home.component.ts
+++ Portfolio2024/src/app/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  ngOnInit(): void {
+    const video = this.el.nativeElement.querySelector('#home-video');
+    if (video) {
+      this.renderer.listen(video, 'mouseenter', () => {
+        video.muted = false;
+      });
+
+      this.renderer.listen(video, 'mouseleave', () => {
+        video.muted = true;
+      })
+    }
+  }
 }
 
