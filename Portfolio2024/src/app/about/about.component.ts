52a73ca626be4887295a3970ba806bc3923e1838
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-11-02 22:24:27 -0300

    Update about.component.ts

--- Portfolio2024/src/app/about/about.component.ts
+++ Portfolio2024/src/app/about/about.component.ts
@@ -9,6 +9,13 @@ export class AboutComponent implements OnInit{
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit(): void {
-    document.
+    const readMoreBtn = this.el.nativeElement.querySelector('.read-more-btn');
+    const aboutMeText = this.el.nativeElement.querySelector('.about-me-text');
+    if (readMoreBtn && aboutMeText) {
+      this.renderer.listen(readMoreBtn, 'click', () => {
+        aboutMeText.classList.toggle('expanded');
+        readMoreBtn.textContent = aboutMeText.classList.contains('expanded') ? 'Mostrar Menos' : 'Saber Mas';
+      });
+    }
   }
 }
