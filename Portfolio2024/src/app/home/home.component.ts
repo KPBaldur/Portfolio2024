4fc3d86a890c3fa5fc0be6e791e729c6721c686b
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-11-09 09:17:25 -0300

    Update home.component.ts

--- Portfolio2024/src/app/home/home.component.ts
+++ Portfolio2024/src/app/home/home.component.ts
@@ -3,22 +3,35 @@ import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
+  isMuted: boolean = false;
+
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit(): void {
     const video = this.el.nativeElement.querySelector('#home-video');
     if (video) {
-      this.renderer.listen(video, 'mouseenter', () => {
-        video.muted = false;
-      });
+      video.volume = 0.5; // Comienza con un volumen medio
+      video.muted = false; // Desmutear el video por defecto
+    }
+  }
 
-      this.renderer.listen(video, 'mouseleave', () => {
-        video.muted = true;
-      })
+  changeVolume(event: Event): void {
+    const video = this.el.nativeElement.querySelector('#home-video');
+    const input = event.target as HTMLInputElement;
+    if (video) {
+      video.volume = parseFloat(input.value);
+      this.isMuted = video.volume === 0;
+    }
+  }
+
+  toggleMute(): void {
+    const video = this.el.nativeElement.querySelector('#home-video');
+    if (video) {
+      video.muted = !video.muted;
+      this.isMuted = video.muted;
     }
   }
 }
-
