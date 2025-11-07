efdd793e904580e6b40ec69eb9daf49c5e01be13
BaldurDev <kevin.pizarrosanz@gmail.com>
2025-11-07 10:39:27 -0300

    Carga de nuevos proyectos.

--- portfolio2024/src/app/components/portfolio/portfolio.component.ts
+++ portfolio2024/src/app/components/portfolio/portfolio.component.ts
@@ -14,42 +14,32 @@ export class PortfolioComponent {
       tech: 'Python - FastAPI - Uvicorn',
       description: 'Desarrollo de API RESTful con endpoints para capítulos, personajes y temporadas. Proyecto orientado a la comunidad Open Source para libre uso a los fans de la serie.',
       images: [
-        '../../../assets/img/RegularShow.png',
-        '../../../assets/img/RegularShowAPI.png',
-        '../../../assets/img/RegularShowAPI2.png'
+        'assets/img/RegularShow.png',
+        'assets/img/RegularShowAPI.png',
+        'assets/img/RegularShowAPI2.png'
       ],
       repo: 'https://github.com/kevinp/regularshow-api',
       demo: 'https://regularshow-api.onrender.com'
     },
     {
-      title: 'Unreal Engine Project',
-      tech: 'Unreal Engine - C++',
-      description: 'Desarrollo de mecánicas y sistemas de IA con Unreal Engine 5.',
+      title: 'Soluciones Luis Maldonado',
+      tech: 'HTML - CSS - JS',
+      description: 'Landing page para cliente Soluciones Luis Maldonado, sitio web de presentacion de servicios de construccion para zonas rurales, enfocado en accesibilidad y diseño UX. Segun requerimientos del cliente, solicito un diseño simple y comodo especial para perseonas payores de edad con poco conocimiento en tecnologia, formularios listos para contactar rapido y facil. Landingpage esta esperando aprobacion de cotizacion de hosting para su fase a productivo',
       images: [
-        'assets/img/unreal1.png',
-        'assets/img/unreal2.png'
+        'assets/img/SolucionesLMaldonado/SolucionesLMaldonado01.png',
+        'assets/img/SolucionesLMaldonado/SolucionesLMaldonado02.png',
+        'assets/img/SolucionesLMaldonado/SolucionesLMaldonado03.png',
       ],
-      repo: '',
-      demo: ''
+      demo: 'https://kpbaldur.github.io/LuisMaldonadoSoluciones'
     },
     {
-      title: 'Fundación Rescata',
-      tech: 'HTML - CSS - JS',
-      description: 'Sitio web para adopción de animales, enfocado en accesibilidad y diseño UX.',
+      title: 'TodoAppRoll v2.0 - Gestion de tareas con alarmas',
+      tech: 'NodeJS - Express - React - Vite',
+      description: 'Aplicacion Web para poder gestionar y crear alarmas para tareas y eventos, con notificaciones en tiempo real junto con administrador multimedia para personalizar las alarmas con un popup de notificacion. La aplicacion esta desarrollada para funcionar, de forma local como una solucion temporal para gestionar alarmas y tareas, con la posibilidad de expandirla en el futuro para ser utilizada en servidores y dispositivos remotos. La version 3 de TodoAppRoll sera una aplicacion de escritorio para poder gestionar de forma mas diversa las tareas y su uso.',
       images: [
-        'assets/img/rescata1.png',
-        'assets/img/rescata2.png'
-      ],
-      repo: 'https://github.com/kevinp/fundacion-rescata',
-      demo: 'https://fundacion-rescata.cl'
-    },
-    {
-      title: 'Fundación Rescata',
-      tech: 'HTML - CSS - JS',
-      description: 'Sitio web para adopción de animales, enfocado en accesibilidad y diseño UX.',
-      images: [
-        'assets/img/rescata1.png',
-        'assets/img/rescata2.png'
+        'assets/img/TodoAppRollv02/TodoAppRollV02.png',
+        'assets/img/TodoAppRollv02/TodoAppRollV02-02.png',
+        'assets/img/TodoAppRollv02/TodoAppRollV02-03.png'
       ],
       repo: 'https://github.com/kevinp/fundacion-rescata',
       demo: 'https://fundacion-rescata.cl'
